17c066cb7bbc3dfb3e4f98e7cabba807b4539982
elliote.wang <15091535460@163.com>
2017-07-27 14:31:30 +0800

    添加了首页测试数据的展示

--- www/handlers.py
+++ www/handlers.py
@@ -2,3 +2,14 @@
 # -*- coding: utf-8 -*-
 
 ' url handlers '
+from www.coreweb import get
+from www.models import User
+
+
+@get('/')
+async def index(request):
+    users = await User.findAll()
+    return {
+        '__template__': 'test.html',
+        'users': users
+    }
