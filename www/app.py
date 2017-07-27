a29838c82df7352a5747aa2aa6d01f2da7631e9c
elliote.wang <15091535460@163.com>
2017-07-27 16:09:30 +0800

    添加模拟的blogs数据的显示

--- www/app.py
+++ www/app.py
@@ -86,15 +86,15 @@ async def response_factory(app, handler):
 def datetime_filter(t):
     delta = int(time.time() - t)
     if delta < 60:
-        return u'1分钟前'
+        return '1分钟前'
     if delta < 3600:
-        return u'%s分钟前' % (delta // 60)
+        return '%s分钟前' % (delta // 60)
     if delta < 86400:
-        return u'%s小时前' % (delta // 3600)
+        return '%s小时前' % (delta // 3600)
     if delta < 604800:
-        return u'%s天前' % (delta // 86400)
+        return '%s天前' % (delta // 86400)
     dt = datetime.fromtimestamp(t)
-    return u'%s年%s月%s日' % (dt.year, dt.month, dt.day)
+    return '%s年%s月%s日' % (dt.year, dt.month, dt.day)
 
 
 def init_jinja2(app, **kw):
@@ -120,7 +120,7 @@ def init_jinja2(app, **kw):
 
 
 async def init(loop):
-    await orm.create_pool(loop=loop, host='127.0.0.1', port=3306, user="root", db="webapp")
+    await orm.create_pool(loop=loop, host='localhost', port=3306, user="root", db="webapp")
     app = web.Application(loop=loop, middlewares=[logger_factory, response_factory])
     init_jinja2(app, filters=dict(datetime=datetime_filter))
     add_routes(app, 'handlers')
--- www/handlers.py
+++ www/handlers.py
@@ -1,15 +1,31 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
+import time
 
-' url handlers '
 from www.coreweb import get
-from www.models import User
+from www.models import User, Blogs
 
+
+# @get('/')
+# async def index(request):
+#     users = await User.findAll()
+#     return {
+#         '__template__': 'test.html',
+#         'users': users
+#     }
 
 @get('/')
 async def index(request):
-    users = await User.findAll()
+    summary = 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
+    blogs = [
+        Blogs(id='1', name='Test Blog', summary=summary, created_at=time.time() - 120),
+        Blogs(id='2', name='Something New', summary=summary, created_at=time.time() - 3600),
+        Blogs(id='3', name='Learn Swift', summary=summary, created_at=time.time() - 7200)
+    ]
     return {
-        '__template__': 'test.html',
-        'users': users
+        '__template__': 'blogs.html',
+        'blogs': blogs
     }
+
+
+
